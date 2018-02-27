56008dcb4b65ba467e4713c6b69953ea78a70ed7
ericbli <ericbli.se@gmail.com>
2018-02-27 13:58:13 +0100

    first commit

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark navbar-light mb-1">
+   
+  <a class="navbar-brand" href="#">Logo</a>
+    <div class="container">
+      <a href="#" class="navbar-brand">Larticles</a>
+    
+     <form class="form-inline" action="/action_page.php">
+    <input class="form-control mr-sm-5" type="text" placeholder="Search">
+    <button class="btn btn-success" type="submit">Search</button>
+  </form>
+  </div>
+  </nav>
+</template>
